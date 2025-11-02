dfcf4b27b440f70d06d4156489fe2d9e53f12550
Phu Thanh <pthanh137@gmail.com>
2025-11-02 13:21:56 +0700

    #28

--- lms_frontend/src/components/Teacher/TeacherRegister.js
+++ lms_frontend/src/components/Teacher/TeacherRegister.js
@@ -1,16 +1,67 @@
 // Import Link để điều hướng giữa các trang React
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
-// Component Register hiển thị form đăng ký người dùng
+import { useEffect, useState } from "react";
+import axios from "axios";
+const BASE_URL = 'http://127.0.0.1:8000/api/teacher/';
 function TeacherRegister() {
-  useEffect(()=>{
-    document.title ='Teacher Register';
+  const [teacherData, setteacherData] = useState({
+    'full_name': '',
+    'email': '',
+    'password': '',
+    'qualification': '',
+    'mobile_no': '',
+    'skills': '',
+    'status': ''
   });
+  //Chang element value
+  const handleChange = (event) => {
+    setteacherData({
+      ...teacherData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  //End
+
+  // Submit Form
+  const submitForm = (event) => {
+    event.preventDefault();
+    const teacherFormData = new FormData();
+    teacherFormData.append("full_name", teacherData.full_name);
+    teacherFormData.append("email", teacherData.email);
+    teacherFormData.append("password", teacherData.password);
+    teacherFormData.append("qualification", teacherData.qualification);
+    teacherFormData.append("mobile_no", teacherData.mobile_no);
+    teacherFormData.append("skills", teacherData.skills);
+
+    try {
+      axios.post(BASE_URL, teacherFormData).then((response) => {
+        setteacherData({
+          'full_name': '',
+          'email': '',
+          'password': '',
+          'qualification': '',
+          'mobile_no': '',
+          'skills': '',
+          'status': 'success'
+        })
+      });
+    } catch (error) {
+      console.log(error);
+      setteacherData({'status':'error'})
+    }
+
+  };
+
+  // End
+
   return (
     // Khung chính của form (Bootstrap)
     <div className="container mt-4">
       <div className="row">
         <div className="col-6 offset-3">
+          {teacherData.status =='success' && <p class="text-success">Thanks for your registeration</p>}
+          {teacherData.status =='error'&& <p class="text-danger">Something wrong happened!</p>}
           <div className="card">
             <h5 className="card-header">Teacher Register</h5>
 
@@ -22,7 +73,7 @@ function TeacherRegister() {
                   <label for="exampleInputEmail1" className="form-label">
                     Full Name
                   </label>
-                  <input type="text" className="form-control" />
+                  <input value={teacherData.full_name} onChange={handleChange} name='full_name' type="text" className="form-control" />
                 </div>
 
                 {/* Email */}
@@ -30,13 +81,13 @@ function TeacherRegister() {
                   <label for="exampleInputEmail1" className="form-label">
                     Email
                   </label>
-                  <input type="email" className="form-control" />
+                  <input value={teacherData.email}onChange={handleChange} name='email' type="email" className="form-control" />
                 </div>
                 <div className="mb-3">
                   <label for="exampleInputPassword1" className="form-label">
                     Password
                   </label>
-                  <input
+                  <input value={teacherData.password}onChange={handleChange} name='password'
                     type="password"
                     className="form-control"
                     id="exampleInputPassword1"
@@ -46,14 +97,14 @@ function TeacherRegister() {
                   <label for="exampleInputEmail1" className="form-label">
                     Qualification
                   </label>
-                  <input type="text" className="form-control" />
+                  <input value={teacherData.qualification} onChange={handleChange} type="text" name="qualification" className="form-control" />
                 </div>
 
                 <div className="mb-3">
                   <label for="exampleInputEmail1" className="form-label">
                     Mobile Number
                   </label>
-                  <input type="number" className="form-control" />
+                  <input value={teacherData.mobile_no} onChange={handleChange} type="number" name="mobile_no" className="form-control" />
                 </div>
 
                 {/* Interests */}
@@ -61,14 +112,14 @@ function TeacherRegister() {
                   <label for="exampleInputEmail1" className="form-label">
                     Skills
                   </label>
-                  <textarea className="form-control"></textarea>
+                  <textarea value={teacherData.skills} onChange={handleChange} name="skills" className="form-control"></textarea>
                   <div id="emailHelp" className="form-text">
                     Php, Python, Javascript, etc
                   </div>
                 </div>
 
                 {/* Nút Register */}
-                <button type="submit" className="btn btn-primary">
+                <button onClick={submitForm} type="button" className="btn btn-primary">
                   Register
                 </button>
               </form>
