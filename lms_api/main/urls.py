66a95d56a6a039de50bd124c2f397760e0233b2f
Phu Thanh <pthanh137@gmail.com>
2025-11-09 22:21:16 +0700

    #55

--- lms_api/main/urls.py
+++ lms_api/main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     #Teacher
     path('teacher/', views.TeacherList.as_view()),
     path('teacher/<int:pk>/', views.TeacherDetail.as_view()),
+    path('teacher/change-password/<int:teacher_id>/', views.teacher_change_password),
     path('teacher-login/', views.teacher_login),
     #Category
     path('category/', views.CategoryList.as_view()),
--- lms_frontend/src/components/Teacher/TeacherChangePassword.js
+++ lms_frontend/src/components/Teacher/TeacherChangePassword.js
@@ -1,35 +1,85 @@
-// Import Link để điều hướng giữa các trang trong React
 import { Link } from "react-router-dom";
-// Import Sidebar để hiển thị menu bên trái
 import TeacherSidebar from "./TeacherSidebar";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+const BASE_URL = 'http://127.0.0.1:8000/api/';
 
-// Component ProfileSetting: hiển thị trang cài đặt thông tin người dùng
 function ChangePassword() {
+  const [teacherData, setteacherData] = useState({
+    'password': '',
+
+  });
+  const teacherId = localStorage.getItem('teacherId');
+  const handleChange = (event) => {
+    setteacherData({
+      ...teacherData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  // Submit Form
+  const submitForm = (event) => {
+    event.preventDefault();
+    const teacherFormData = new FormData();
+    teacherFormData.append("password", teacherData.password);
+   
+    try {
+      axios.post(BASE_URL + 'teacher/change-password/' + teacherId + '/', teacherFormData)
+      .then((response) => {
+        if (response.status === 200) {
+            Swal.fire({
+                      title: 'Data has been updated successfully!',
+                      icon: 'success',
+                      toast: true,
+                      timer: 10000,
+                      position: 'top-right',
+                      timerProgressBar: true,
+                      showConfirmButton: false
+                    });
+          window.location.href = 'teacher-logout';
+        }else{
+          alert('Oop... Some error occured');
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      setteacherData({ 'status': 'error' })
+    }
+
+  };
+
+  useEffect(() => {
+    document.title = "Teacher Change Password"
+  });
+  const teacherLoginStatus = localStorage.getItem('teacherLoginStatus')
+  if (teacherLoginStatus !== 'true') {
+    window.location.href = 'teacher-login';
+  }
   return (
-    // Bao toàn bộ nội dung trong khung container Bootstrap
+
     <div className="container mt-4">
       <div className="row">
-        {/* Cột bên trái: hiển thị menu điều hướng Dashboard */}
         <aside className="col-md-3">
           <TeacherSidebar />
         </aside>
-
-        {/* Cột bên phải: hiển thị nội dung chính của trang (Profile Setting) */}
         <section className="col-md-9">
           <div className="card">
             <h5 className="card-header">ChangePassword</h5>
             <div className="card-body">
+              
               <div class="mb-3 row">
                 <label for="staticEmail" class="col-sm-2 col-form-label">
                   New Password
                 </label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" id="staticEmail" />
+                  <input type="text" name="password" value={teacherData.password} onChange={handleChange} class="form-control" id="staticEmail" />
                 </div>
               </div>
 
               <hr />
-              <button className="btn btn-primary">Update</button>
+              <button className="btn btn-primary" onClick={submitForm}>Update</button>
             </div>
           </div>
         </section>
@@ -38,5 +88,4 @@ function ChangePassword() {
   );
 }
 
-// Xuất component để có thể import và sử dụng ở nơi khác (App.js, Router, v.v.)
 export default ChangePassword;
